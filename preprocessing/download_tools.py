4fafdbcb6a30a267650c807030b5e7c05d5f05e1
lsh-159 <110099021+lsh-159@users.noreply.github.com>
2023-10-18 13:55:28 +0900

    comment

--- preprocessing/download_tools.py
+++ preprocessing/download_tools.py
@@ -10,7 +10,16 @@ import wget
 
 BASE_URL = "https://dl.fbaipublicfiles.com/atlas"
 
-
+"""
+    (wget)Download a file from the source URL to the target path if it doesn't already exist.
+    
+    Args:
+        source (str): The URL of the file to download.
+        target (str): The local path where the file should be saved.
+        
+    Returns:
+        None
+"""
 def maybe_download_file(source, target):
     if not os.path.exists(target):
         os.makedirs(os.path.dirname(target), exist_ok=True)
@@ -18,10 +27,28 @@ def maybe_download_file(source, target):
         wget.download(source, out=str(target))
         print()
 
-
+"""
+    Construct the full URL for a file given its path relative to the BASE_URL.
+    
+    Args:
+        path (str): The relative path of the file.
+        
+    Returns:
+        str: The full URL of the file.
+ """
 def get_s3_path(path):
     return f"{BASE_URL}/{path}"
 
 
+"""
+    Construct the local path where a file should be saved.
+    
+    Args:
+        output_dir (str): The base directory where files should be saved.
+        path (str): The relative path of the file.
+        
+    Returns:
+        str: The full local path where the file should be saved.
+ """
 def get_download_path(output_dir, path):
     return os.path.join(output_dir, path)
